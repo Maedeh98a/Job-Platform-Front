ab312040d22d61963b2a7f3949e682ef7716a891
Maedeh98a <maedeh.ahmadian98@gmail.com>
2025-06-12 10:37:24 +0200

    add new changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+// import {github} from "../src/assets/github.png"
+// import {linkedin} from "../src/assets/linkedin.png"
 function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div id='footer-style'>
+        <Link to="https://www.linkedin.com/in/maedeh-ahmadian">
+      <img src="../src/assets/linkedin.png" alt="linkedin"/>
+      </Link>
+      <Link to="https://github.com/Maedeh98a">
+      <img src="../src/assets/github.png" alt="linkedin"/>
+      </Link>
+
+      </div>
+      
+    </footer>
   )
 }
 
